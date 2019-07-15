0f357cfc0d0eb08e4e86fbf0b56e642d6bcc07d5
Erik <hall.erik@gmail.com>
2019-07-14 21:24:09 -0600

    more tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -93,4 +93,178 @@ class RecipeListCreateTests(TestCase):
 
 
 class RecipeRetrieveUpdateDestroyTests(TestCase):
-    pass
+    def test_anon_can_retrieve(self):
+        '''
+        Anon users can retrieve single recipes.
+        '''
+        user = User.objects.create_user(username='steve')
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=user)
+        serializer = RecipeSerializer(recipe)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_owner_can_retrieve(self):
+        '''
+        Recipe owner can retrieve recipe.
+        '''
+        user = User.objects.create_user(username='steve')
+        client = APIClient()
+        client.force_authenticate(user=user)
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=user)
+        serializer = RecipeSerializer(recipe)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_non_owner_user_can_retrieve(self):
+        '''
+        Authenticated user whomst does not own
+        recipe can retrieve.
+        '''
+        owner = User.objects.create_user(username='steve')
+        user = User.objects.create_user(username='bront')
+        client = APIClient()
+        client.force_authenticate(user=user)
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=owner)
+        serializer = RecipeSerializer(recipe)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_anon_cant_update(self):
+        '''
+        Anon user cannot update recipes.
+        '''
+        owner = User.objects.create_user(username='steve')
+        client = APIClient()
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=owner)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.put(url, {
+            'title': 'soup',
+            'desciption': 'a nice stew',
+            'cook_time': '5',
+            'servings': '12',
+            'ingredients': 'water, beef, celery',
+            'directions': 'cook, eat'
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Recipe.objects.get().title, 'stew')
+
+    def test_non_owner_cant_update(self):
+        '''
+        Non owning user cannot update recipes.
+        '''
+        owner = User.objects.create_user(username='steve')
+        user = User.objects.create_user(username='bront')
+        client = APIClient()
+        client.force_authenticate(user=user)
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=owner)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.put(url, {
+            'title': 'soup',
+            'desciption': 'a nice stew',
+            'cook_time': '5',
+            'servings': '12',
+            'ingredients': 'water, beef, celery',
+            'directions': 'cook, eat'
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Recipe.objects.get().title, 'stew')
+
+    def test_owner_can_update(self):
+        '''
+        Owning user can update recipes.
+        '''
+        owner = User.objects.create_user(username='steve')
+        client = APIClient()
+        client.force_authenticate(user=owner)
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=owner)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.put(url, {
+            'title': 'soup',
+            'desciption': 'a nice stew',
+            'cook_time': '5',
+            'servings': '12',
+            'ingredients': 'water, beef, celery',
+            'directions': 'cook, eat'
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Recipe.objects.get().title, 'soup')
+
+    def test_anon_cant_destroy(self):
+        '''
+        Anon user cannot destroy recipes.
+        '''
+        user = User.objects.create_user(username='steve')
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=user)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Recipe.objects.count(), 1)
+
+    def test_non_onwer_cant_destroy(self):
+        '''
+        Non onwer user cannot destroy recipes.
+        '''
+        owner = User.objects.create_user(username='steve')
+        user = User.objects.create_user(username='bront')
+        client = APIClient()
+        client.force_authenticate(user=user)
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=owner)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Recipe.objects.count(), 1)
+
+    def test_onwer_can_destroy(self):
+        '''
+        Onwer can destroy recipes.
+        '''
+        user = User.objects.create_user(username='steve')
+        client = APIClient()
+        client.force_authenticate(user=user)
+        recipe = Recipe.objects.create(
+            title='stew', description='a nice stew',
+            cook_time='5', servings='12',
+            ingredients='water, beef, celery',
+            directions='cook, eat', user=user)
+        url = reverse('recipe_rud', kwargs={'id': recipe.id})
+        response = client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Recipe.objects.count(), 0)
