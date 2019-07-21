582bd4e52e5b230544591f650b2d03832fa0bdb7
Erik <hall.erik@gmail.com>
2019-07-21 09:31:43 -0600

    better url pattern for home view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
     path('api/recipes/',
          views.RecipeListCreate.as_view(), name='recipe_list_create'),
     path('api/recipes/<id>', views.RecipeRUD.as_view(), name='recipe_rud'),
@@ -13,4 +14,6 @@ urlpatterns = [
     path('api/register/', views.RegisterView.as_view(), name='api_register'),
     path('api/login/', obtain_auth_token, name='api_token_auth'),
     path('api/user/', views.CurrentUserView.as_view(), name='current_user'),
+    url(r'^.*',
+        TemplateView.as_view(template_name='recipes/index.html'), name='home'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import (
@@ -15,11 +13,6 @@ from .permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 from .models import Recipe, MadeRecipe
 from .serializers import UserSerializer, RecipeSerializer
-
-
-class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'recipes/index.html', context=None)
 
 
 class RecipeListCreate(ListCreateAPIView):
