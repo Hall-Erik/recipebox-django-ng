a22abdb8f3cb79cf5121a372c04e95800f6afda6
Erik <hall.erik@gmail.com>
2019-07-30 10:11:14 -0600

    reactive register/login forms

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -9,20 +9,26 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-  public username: string = '';
-  public password: string = '';
+  get username() { return this.loginForm.get('username'); }
+  get password() { return this.loginForm.get('password'); }
 
   constructor(
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) { }
 
   login() {
-    this.userService.login(this.username, this.password)
+    this.userService.login(this.username.value, this.password.value)
       .subscribe(() => {
-        console.log('Sucessfully logged in.');
         this.router.navigate(['index']);
+      }, () => {
+        // do something couldn't log in
       });
   }
 }
--- src/app/users/register/register.component.ts
+++ src/app/users/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -8,21 +9,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  public registerForm = this.fb.group({
+    username: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  }, {validator: this.passwords_match});
 
-  public username: string = '';
-  public email: string = '';
-  public password: string = '';
-  public confirmPassword: string = '';
+  get username() { return this.registerForm.get('username'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
 
   constructor(
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) { }
 
+  passwords_match(c: AbstractControl) {
+    if (c.get('password').value !== c.get('confirmPassword').value) {
+      return {invalid: true};
+    }
+  }
+
   register() {
-    this.userService.register(this.username, this.email, this.password)
-      .subscribe(() => {
+    this.userService.register(
+      this.username.value, this.email.value, this.password.value)
+      .subscribe((resp) => {
         this.router.navigate(['login']);
+      }, (err) => {
+        if (err.error.username) {
+          // do something (a user with that name exists.)
+        }
       });
   }
 }
