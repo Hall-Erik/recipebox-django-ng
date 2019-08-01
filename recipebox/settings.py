86a84456ecea6de3ef32c3d195fc06f1cbb89250
Erik <hall.erik@gmail.com>
2019-08-01 08:32:37 -0600

    disable admin site

--- recipebox/settings.py
+++ recipebox/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
