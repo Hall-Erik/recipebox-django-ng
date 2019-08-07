95987045b49c522daddfb820d4099cda4319c30f
Erik <hall.erik@gmail.com>
2019-08-07 10:47:42 -0600

    pep8

--- recipes/apps.py
+++ recipes/apps.py
@@ -5,4 +5,4 @@ class RecipesConfig(AppConfig):
     name = 'recipes'
 
     def ready(self):
-        import recipes.signals
+        import recipes.signals  # noqa: F401
--- recipes/signals.py
+++ recipes/signals.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.dispatch import receiver
 from django.template.loader import render_to_string
-from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 
 
@@ -17,14 +16,16 @@ def password_reset_token_created(
         'reset_password_url': f'{domain}/reset/{reset_password_token.key}'
     }
 
-    email_html_message = render_to_string('email/user_reset_password.html', context)
-    email_txt_message = render_to_string('email/user_reset_password.txt', context)
+    email_html_message = render_to_string(
+      'email/user_reset_password.html', context)
+    email_txt_message = render_to_string(
+      'email/user_reset_password.txt', context)
 
     msg = EmailMultiAlternatives(
         "Password Reset for recipe_box",
         email_txt_message,
         'noreply@recipebox',
         [reset_password_token.user.email])
-    
+
     msg.attach_alternative(email_html_message, 'txt/html')
     msg.send()
